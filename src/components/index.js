4610e80822ba8edcf27d1f35acd424ee6c96a2b2
Kangho <kkh4323@naver.com>
2024-06-29 11:11:29 +0900

    apply React Router

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import MovieList from "./MovieList"
+import TvList from "./TVList"
+
+export { MovieList, TvList }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 

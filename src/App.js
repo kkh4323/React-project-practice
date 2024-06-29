05875049a8a3088a4d4dcc328dc6526a090868e3
Kangho <kkh4323@naver.com>
2024-06-29 10:20:28 +0900

    first commit
    add MovieList, TvList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MovieList from "./MovieList";
+import TvList from "./TVList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <TvList/>
+    );
+};
 
 export default App;
